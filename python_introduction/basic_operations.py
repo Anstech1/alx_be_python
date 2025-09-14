cf0d422729ac80e8865d3141ec23d0f068618560
Josef Ansah Odum <odumjosef4@gmail.com>
2025-09-14 16:08:35 +0000

    Perform simple addition, subtraction and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# Define two variables with integer values
+number1 = 10
+number2 = 5
+# Perform addition of number1 and number2
+print("Addition:", number1 + number2)
+# Perform subtraction of number1 and number2
+print("Subtraction:", number1 - number2)
+# Perform multiplication of number1 and number2
+print("Multiplication:", number1 * number2)
+
+
+  
